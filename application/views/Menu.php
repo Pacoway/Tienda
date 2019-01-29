eeec4768358561482590102da802ce8556b5034f
Paco <pakoway_@hotmail.com>
2019-01-29 09:15:19 +0100

    Initial commit

---
+++ application/views/Menu.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark float-right">
+    <ul class="navbar-nav">
+        <li class="nav-item"><?php echo anchor('Inicio', 'Inicio'); ?></li>
+        <li class="nav-item"><?php echo anchor('Categorias', 'Categorias'); ?></li>
+        <li class="nav-item"><?php echo anchor('Inicio', 'Inicio'); ?></li>
+    </ul>
+</nav>
