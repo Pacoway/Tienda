1218bcc7e6718ecefd5515867ab2293fb4af0a0c
Paco <pakoway_@hotmail.com>
2019-02-04 14:27:58 +0100

    cambios

--- application/views/Menu.php
+++ application/views/Menu.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark float-right">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <ul class="navbar-nav">
-        <li class="nav-item"><?php echo anchor('Inicio', 'Inicio'); ?></li>
-        <li class="nav-item"><?php echo anchor('Categorias', 'Categorias'); ?></li>
-        <li class="nav-item"><?php echo anchor('Inicio', 'Inicio'); ?></li>
+        <li class="nav-item"><span class="nav-link"><?php echo anchor('Inicio', 'Inicio'); ?></span></li>
+        <li class="nav-item"><span class="nav-link"><?php echo anchor('Categorias', 'Categorias'); ?></span></li>
+        <li class="nav-item"><span class="nav-link"><?php echo anchor('Inicio', 'Inicio'); ?></span></li>
     </ul>
 </nav>
