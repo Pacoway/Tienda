f211b291c4f6179264fb443077709129b5a7e74b
Pacoway <pakoway_@hotmail.com>
2019-02-11 13:14:29 +0100

    Principio de Login

--- application/controllers/InicioSesion.php
+++ application/controllers/InicioSesion.php
@@ -15,4 +15,37 @@ class InicioSesion extends CI_Controller {
 	);
 	}
 
+	public function LogIn(){
+		$this->load->model('model_productos');
+		$this->load->model('model_Login');
+
+		if ($this->model_Login->LogOK($this->input->post('usuario'), $this->input->post('contrasena'))) {
+			
+			//crear sesiones
+
+			$datos_categorias['categorias']= $this->model_productos->getCategorias();
+
+			$this->load->view('plantilla', [
+				'titulo' => 'Inicio de sesion',
+				'menu'=>  $this->load->view('menu', $datos_categorias, true),
+				'cuerpo' => $this->load->view('InicioSesion',[],true)
+			 ]
+		);
+
+		} else {
+			
+			$datos_categorias['categorias']= $this->model_productos->getCategorias();
+
+			$this->load->view('plantilla', [
+				'titulo' => 'Inicio de sesion',
+				'menu'=>  $this->load->view('menu', $datos_categorias, true),
+				'cuerpo' => $this->load->view('InicioSesion',[],true)
+			 ]
+		);
+
+		}
+		
+
+	}
+
 }
--- application/views/InicioSesion.php
+++ application/views/InicioSesion.php
@@ -1,12 +1,10 @@
 <?php echo validation_errors(); ?>
-<?php echo form_open('formulario'); ?>
+<?php echo form_open('InicioSesion/LogIn'); ?>
+<h2>Iniciar sesión</h2>
 <h5>Usuario</h5>
 <input type="text" name="username" value="" size="50" />
 <h5>Contraseña</h5>
 <input type="text" name="password" value="" size="50" />
-<h5>Confirmar contraseña</h5>
-<input type="text" name="passconf" value="" size="50" />
-<h5>Email</h5>
-<input type="text" name="email" value="" size="50" />
-<div><input type="submit" value="Enviar" /></div>
+
+<div><input type="submit" value="Iniciar sesion" /></div>
 </form>
